e7eb7804c52bd3d9851b975407d8ec1aa310ea64
dewaary <dewa.septiadi@ptgue.com>
2024-09-14 20:41:22 +0800

    Setup React Native Enviroment

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>Navigation</Text>
+    </View>
+  )
+}
+
+export default Navigation
+
+const styles = StyleSheet.create({})
